481c74465d6c8c7b9ca9f33b1dea462a0e362d99
ayushgupta0010 <ayushgupta0010@gmail.com>
2026-01-17 18:22:47 -0500

    Add dotenv

--- backend/src/bin/main.rs
+++ backend/src/bin/main.rs
@@ -1,8 +1,11 @@
 use backend::app::App;
+use dotenv::dotenv;
 use loco_rs::cli;
 use migration::Migrator;
 
 #[tokio::main]
 async fn main() -> loco_rs::Result<()> {
+    dotenv().ok();
+
     cli::main::<App, Migrator>().await
 }
